5867f287728aeed66aa94a938dfd0ebdfb837bbf
elshadayday <113993301+elshadayday@users.noreply.github.com>
2023-08-06 14:44:09 +0300

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/10-best_score.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
-        return None
-    return max(a_dictionary.items(), key=lambda n: n[1])[0]
